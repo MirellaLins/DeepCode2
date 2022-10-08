d79479c17128c7cad97204086a5bf4e19516f30e
Mirella <mlds.aulas@gmail.com>
2022-10-07 21:04:27 -0300

    Tela completa

--- DeepCode/app/src/main/java/com/domain/deepcode/MainActivity.java
+++ DeepCode/app/src/main/java/com/domain/deepcode/MainActivity.java
@@ -3,12 +3,39 @@ package com.domain.deepcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int contadorDeClicks = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btToast = findViewById(R.id.btToast);
+
+        // PEGAR A REFERÊNCIA DO BOTÃO E TO TEXTVIEW
+
+        Button mCount = findViewById(R.id.btCount);
+
+        TextView mTvNumber = findViewById(R.id.tvNumber);
+        mTvNumber.setText("0");
+
+        btToast.setOnClickListener(view -> {
+            Toast.makeText(this, "Hello Toast" + contadorDeClicks, Toast.LENGTH_LONG).show();
+        });
+
+        // COLOCAR UM CLICK NO BOTÃO
+        mCount.setOnClickListener(view -> {
+
+            contadorDeClicks += 1;
+            mTvNumber.setText(String.valueOf(contadorDeClicks));
+
+        });
+
     }
 }
